8afc9a88ee17f18afece020d9b723d8c2caf6dd4
Enrique Castellón <enrcasher@alu.edu.gva.es>
2025-12-12 17:37:04 +0100

    QueryStrings Para datos en php;

--- pong/formulario_ctl.php
+++ pong/formulario_ctl.php
@@ -7,11 +7,9 @@
     $playerTwo = $_GET['name2'];
 
     if($jugadores==1){
-        header("Location: pong.html");
-        
+        header("Location: pong.html?control=".$control.",velocidad=".$velocidad.",nombre=".$playerOne);
     }else{
-        header("Location: pong2.html");
-        
+        header("Location: pong2.html?control=".$control.",velocidad=".$velocidad.",nombre1=".$playerOne.",nombre2=".$playerTwo);
     }
 exit();
 
